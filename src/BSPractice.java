a0497d5c4c9adab2bf04427d17106bba4d41ff54
Raymond Marx <102569357+RMarx1456@users.noreply.github.com>
2025-04-29 21:25:12 +0000

    Classroom work.

--- src/BSPractice.java
+++ src/BSPractice.java
@@ -1,5 +1,36 @@
 public class BSPractice {
     public static void main(String[] args) {
-        
+        items.add(5);
+        items.add(9);
+        items.add(22);
+        items.add(22);
+
+        insert(items, 15);
+        insert(items, 51);
+        insert(items, 22);
+        insert(items, 23);
+        insert(items, -3);
+    }
+    public static void insert(List<Integer> items, int toInsert) {
+        int low = 0;
+        int high = items.size();
+
+        int mid;
+
+        while(low < high) {
+            mid = low + (high - low)/2;
+
+            int midItem = items.get(mid);
+
+            if(toIner == midItem) {
+                items.add(mid, toInsert);
+                return;
+            } else if(toInsert < midItem) {
+                high = mid;
+            }
+            else (
+                low = mid + 1;
+            )
+        }
     }
 }
