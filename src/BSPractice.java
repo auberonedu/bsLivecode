ad382c5b35e5435d4cd0445e36ca2fd81f670913
Kimberly Mageary <kimberlymageary@gmail.com>
2025-04-29 14:48:13 -0700

    Finished the live code

--- src/BSPractice.java
+++ src/BSPractice.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BSPractice {
     public static void main(String[] args) {
-        
+        List<Integer> items = new ArrayList<>();
+        items.add(5);
+        items.add(7);
+        items.add(8);
+        items.add(15);
+        items.add(23);
+        items.add(35);
+        items.add(56);
+
+        insert(items, 15);
+        System.out.println("Hello world");
+        System.out.println(items);
+    }
+
+    public static void insert(List<Integer> items, int toInsert){
+        int low = 0;
+        int high = items.size();
+
+        int mid;
+
+        while(low<high){
+            mid = low + (high-low) /2;
+
+            int midItem = items.get(mid);
+            if(midItem == toInsert){
+                items.add(mid, toInsert);
+            }else if(toInsert < midItem){
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        items.add(low, toInsert);
     }
 }
